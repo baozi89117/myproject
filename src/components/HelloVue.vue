4016578babde572024c0d282378f04b6e78355f0
zengqi <413689558@qq.com>
2020-04-14 14:19:56 +0800

    change

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -1,22 +1,25 @@
-<!--  -->
 <template>
-  <div>321312123</div>
+  <div>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloBaby',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+
     }
   },
-  // 生命周期 - 创建完成（访问当前this实例）
-  created () {},
-  // 生命周期 - 挂载完成（访问DOM元素）
-  mounted () {}
+  methods: {
+
+  },
+  components: {
+
+  }
 }
 </script>
+
 <style scoped>
-/* @import url(); 引入css类 */
+
 </style>
